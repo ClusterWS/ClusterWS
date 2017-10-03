50f7f444822d40200f479f20b233ec1e065950bf
Dmitrii Goriunov <goriunovd@gmail.com>
2017-10-03 20:51:57 +1300

    Added encode messages function

--- src/modules/common/messages.ts
+++ src/modules/common/messages.ts
@@ -7,3 +7,22 @@ export function processMessage(event: string, data?: any): ProcessMessage {
 export function brokerMessage(channel: string, data?: any): any {
     return JSON.stringify({ channel, data })
 }
+
+
+export function socketEncodeMessages(event: string, data: any, type: string): any {
+    switch (type) {
+        case 'ping': return event
+        case 'emit': return JSON.stringify({ '#': ['e', event, data] })
+        case 'publish': return JSON.stringify({ '#': ['p', event, data] })
+        case 'system': switch (event) {
+            case 'subsribe': return JSON.stringify({ '#': ['s', 's', data] })
+            case 'unsubscribe': return JSON.stringify({ '#': ['s', 'u', data] })
+            case 'configuration': return JSON.stringify({ '#': ['s', 'c', data] })
+        }
+    }
+}
+
+
+export function socketDecodeMessages(event: string, data: any, type: string) {
+
+}
