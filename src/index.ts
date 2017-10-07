50a68d2f08cf34ad2fd45bf0b30519c837ba59fa
Dmitrii Goriunov <goriunovd@gmail.com>
2017-10-07 14:15:36 +1300

    Minor modification , added publish script

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ import { isMaster } from 'cluster'
 import { processWorker } from './modules/worker.process'
 import { processMaster } from './modules/master.process'
 import { Options, UserOptions } from './modules/common/interfaces'
+import { logError } from './modules/common/console'
 
 export class ClusterWS {
-    private static instance: ClusterWS
-
     constructor(configurations: UserOptions) {
-        if (ClusterWS.instance) return
-        ClusterWS.instance = this
+
+        if (!configurations.worker) {
+            logError('Worker must be provided')
+            return
+        }
 
         const options: Options = {
             port: configurations.port || 80,
