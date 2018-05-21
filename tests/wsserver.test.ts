05fc8156c5f6a1ee0a4b17b0cbe9bc59778bb7c1
Dmitrii Goriunov <goriunovd@gmail.com>
2018-05-21 17:48:43 +1200

    Added more test for WSServer.ts file

--- tests/wsserver.test.ts
+++ tests/wsserver.test.ts
@@ -22,4 +22,40 @@ describe('WSServer Functions Tests', () => {
     wss.setBroker(broker, 'myurl');
     wss.publish('channel1', 'test');
   });
+
+  it('Test broadcastMessage function', (done) => {
+    let broker: any = {
+      send: (msg) => {}
+    };
+
+    wss.setBroker(broker, 'myurl');
+    wss.channels.onMany('testchannel', (_, message) => {
+      expect(message).to.equal(`hello world`);
+      done(null);
+    });
+    wss.broadcastMessage(null, Buffer.from(`testchannel%${JSON.stringify({ message: 'hello world' })}`));
+  });
+});
+
+describe('WSServer Broker Resubscribe', () => {
+  let wss = new WSServer();
+  it('Should resubsribe new broker to all channels', (done) => {
+    let resubscribed = 0;
+    let broker: any = {
+      send: (msg) => {
+        if (msg === 'testchannel' || msg === '["testchannel"]') {
+          resubscribed++;
+        }
+
+        if (resubscribed === 2) {
+          done(null);
+        }
+      },
+      readyState: 1
+    };
+
+    wss.setBroker(broker, 'superurl');
+    wss.channels.onMany('testchannel', (_, message) => {});
+    wss.setBroker(broker, 'superurl');
+  });
 });
