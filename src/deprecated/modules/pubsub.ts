fbfe830fc9cdc5d78ba84f3be100e5b7d23737b9
Dmitrii Goriunov <goriunovd@gmail.com>
2019-09-29 22:04:26 +1300

    Move old system in deprecated folder

---
+++ src/deprecated/modules/pubsub.ts
@@ -0,0 +1,160 @@
+import { Listener, Message, Logger } from '../utils/types';
+
+// TODO: Fix serious issue with data reference (in future)
+// TODO: improve performance based on correct pub/sub concept
+export class PubSubEngine {
+  private hooks: { [key: string]: Listener } = {};
+  private users: { [key: string]: Listener } = {};
+  private batches: { [key: string]: Message[] } = {};
+  private channels: { [key: string]: string[] } = {};
+
+  constructor(private logger: Logger, private interval: number, predefinedChannels: any = {}) {
+    this.run();
+    this.channels = predefinedChannels;
+  }
+
+  public addListener(event: string, listener: Listener): void {
+    this.hooks[event] = listener;
+  }
+
+  // get all channels which this server is subscribed to
+  public getChannels(): string[] {
+    return Object.keys(this.channels);
+  }
+
+  // register particular user with listener in pubSub system
+  public register(userId: string, listener: Listener): void {
+    this.users[userId] = listener;
+  }
+
+  // remove user from all subscribed channels (if has) and delete listener
+  public unregister(userId: string, channels: string[]): void {
+    this.logger.debug(`Removing ${userId} from`, channels, 'channels', `(pid: ${process.pid})`);
+    for (let i: number = 0, len: number = channels.length; i < len; i++) {
+      this.unsubscribe(userId, channels[i]);
+    }
+    delete this.users[userId];
+  }
+
+  // subscribe user to specific channel
+  public subscribe(userId: string, channel: string): any {
+    if (!this.users[userId]) {
+      return this.logger.warning(`Trying to subscribe not existing user ${userId}`);
+    }
+
+    if (this.channels[channel]) {
+      return this.channels[channel].push(userId);
+    }
+
+    if (this.hooks['channelAdd']) {
+      this.hooks['channelAdd'](channel);
+    }
+    this.channels[channel] = ['#broker', userId];
+  }
+
+  // remove unsubscribe user from specific channel
+  public unsubscribe(userId: string, channel: string): void {
+    const channelArray: string[] = this.channels[channel];
+    if (channelArray && channelArray.length) {
+      const userIndex: number = channelArray.indexOf(userId);
+      if (userIndex !== -1) {
+        channelArray.splice(userIndex, 1);
+      }
+    }
+
+    // remove channels object if there is no more users
+    if (channelArray && channelArray.length === 1) {
+      if (this.hooks['channelClose']) {
+        this.hooks['channelClose'](channel);
+      }
+      delete this.channels[channel];
+    }
+  }
+
+  // publish message to specific channel (if user id is not proved them publish as anonym)
+  public publish(channel: string, message: Message, userId?: string): any {
+    // check if we have batch available for this channel already
+    let batch: Message[] = this.batches[channel];
+
+    if (!batch) {
+      this.batches[channel] = batch = [];
+    }
+
+    if (message instanceof Array) {
+      for (let i: number = 0, len: number = message.length; i < len; i++) {
+        batch.push({ userId, message: message[i] });
+      }
+    } else {
+      batch.push({ userId, message });
+    }
+  }
+
+  private flush(): void {
+    const preparedMessages: any = {};
+
+    // for each key in batches (key is actual channel name)
+    for (const channel in this.batches) {
+      if (true) {
+        // get all users for that channel
+        const users: string[] = this.channels[channel];
+
+        // get actual messages which were send from last iteration
+        const batch: Message[] = this.batches[channel];
+        const batchLen: number = batch.length;
+        // make sure we actually have users for that channel
+        if (users) {
+          // for each user we need to create separate messages array
+          for (let j: number = 0, userLen: number = users.length; j < userLen; j++) {
+            const userId: string = users[j];
+            const userSpecificMessages: any[] = [];
+
+            // make sure we do not send message to the user if user was actual publisher
+            for (let k: number = 0; k < batchLen; k++) {
+              if (batch[k].userId !== userId) {
+                userSpecificMessages.push(batch[k].message);
+              }
+            }
+
+            // if we have messages for this user then add this channel with message to all messages
+            if (userSpecificMessages.length) {
+              if (!preparedMessages[userId]) {
+                preparedMessages[userId] = {};
+              }
+              preparedMessages[userId][channel] = userSpecificMessages;
+            }
+          }
+        } else {
+          // this scenario only if we trying to send message to the channel
+          // which does not exist in current server but may exist in some other
+          const brokerMessage: any[] = [];
+          for (let i: number = 0; i < batchLen; i++) {
+            brokerMessage.push(batch[i].message);
+          }
+          // check if broker actually exists in prepared messages
+          if (!preparedMessages['#broker']) {
+            preparedMessages['#broker'] = {};
+          }
+          preparedMessages['#broker'][channel] = brokerMessage;
+        }
+      }
+    }
+
+    // clean up all batches
+    this.batches = {};
+
+    // now send message to each user which should receive them
+    for (const userId in preparedMessages) {
+      if (this.users[userId]) {
+        this.users[userId](preparedMessages[userId]);
+      }
+    }
+  }
+
+  // run pub sub engine in loop in specific interval
+  private run(): void {
+    setTimeout(() => {
+      this.flush();
+      this.run();
+    }, this.interval);
+  }
+}
--- src/modules/pubsub.ts
+++ src/modules/pubsub.ts
@@ -1,160 +1,299 @@
-import { Listener, Message, Logger } from '../utils/types';
+// Version 2.0 Pub Sub
 
-// TODO: Fix serious issue with data reference (in future)
-// TODO: improve performance based on correct pub/sub concept
+// TODO: add execution time for each function
+type Listener = (messages: { [key: string]: any[] }) => void;
+
+interface PubSubEngineOptions {
+  sync: boolean;
+  flushInterval: number;
+  onChannelCreated: (channel: string) => void;
+  onChannelDestroyed: (channel: string) => void;
+}
+
+function noop(): void { /** noop function */ }
+
+function findIndexOf(arr: string[], value: string): number {
+  for (let i: number = 0, len: number = arr.length; i < len; i++) {
+    if (arr[i] === value) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+function isObjectEmpty(object: { [key: string]: any }): boolean {
+  if (object.hasOwnProperty('len')) {
+    if (object.len <= 0) {
+      return true;
+    }
+
+    return false;
+  }
+
+  for (const key in object) {
+    if (object.hasOwnProperty(key)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 export class PubSubEngine {
-  private hooks: { [key: string]: Listener } = {};
-  private users: { [key: string]: Listener } = {};
-  private batches: { [key: string]: Message[] } = {};
-  private channels: { [key: string]: string[] } = {};
+  public static GLOBAL_USER: string = '*';
 
-  constructor(private logger: Logger, private interval: number, predefinedChannels: any = {}) {
-    this.run();
-    this.channels = predefinedChannels;
+  private options: PubSubEngineOptions;
+  private usersLink: { [key: string]: { listener: Listener, channels: string[] } } = {};
+  private channelsUsers: { [key: string]: { len: number, [key: string]: string | number } } = {};
+
+  // message handler
+  private channelsBatches: { [key: string]: Array<[string | null, any]> } = {};
+
+  // Flush options
+  private boundFlush: () => void;
+  private flushTimeout: NodeJS.Timeout;
+
+  constructor(options: Partial<PubSubEngineOptions> = {}) {
+    this.options = {
+      sync: false,
+      flushInterval: 10,
+      onChannelCreated: noop,
+      onChannelDestroyed: noop,
+      ...options
+    };
+
+    this.boundFlush = this.flush.bind(this);
   }
 
-  public addListener(event: string, listener: Listener): void {
-    this.hooks[event] = listener;
-  }
+  public publish(channel: string, message: any, user: string = null): void {
+    const channelBatch: Array<[string | null, any]> | undefined = this.channelsBatches[channel];
 
-  // get all channels which this server is subscribed to
-  public getChannels(): string[] {
-    return Object.keys(this.channels);
-  }
-
-  // register particular user with listener in pubSub system
-  public register(userId: string, listener: Listener): void {
-    this.users[userId] = listener;
-  }
-
-  // remove user from all subscribed channels (if has) and delete listener
-  public unregister(userId: string, channels: string[]): void {
-    this.logger.debug(`Removing ${userId} from`, channels, 'channels', `(pid: ${process.pid})`);
-    for (let i: number = 0, len: number = channels.length; i < len; i++) {
-      this.unsubscribe(userId, channels[i]);
-    }
-    delete this.users[userId];
-  }
-
-  // subscribe user to specific channel
-  public subscribe(userId: string, channel: string): any {
-    if (!this.users[userId]) {
-      return this.logger.warning(`Trying to subscribe not existing user ${userId}`);
-    }
-
-    if (this.channels[channel]) {
-      return this.channels[channel].push(userId);
-    }
-
-    if (this.hooks['channelAdd']) {
-      this.hooks['channelAdd'](channel);
-    }
-    this.channels[channel] = ['#broker', userId];
-  }
-
-  // remove unsubscribe user from specific channel
-  public unsubscribe(userId: string, channel: string): void {
-    const channelArray: string[] = this.channels[channel];
-    if (channelArray && channelArray.length) {
-      const userIndex: number = channelArray.indexOf(userId);
-      if (userIndex !== -1) {
-        channelArray.splice(userIndex, 1);
-      }
-    }
-
-    // remove channels object if there is no more users
-    if (channelArray && channelArray.length === 1) {
-      if (this.hooks['channelClose']) {
-        this.hooks['channelClose'](channel);
-      }
-      delete this.channels[channel];
-    }
-  }
-
-  // publish message to specific channel (if user id is not proved them publish as anonym)
-  public publish(channel: string, message: Message, userId?: string): any {
-    // check if we have batch available for this channel already
-    let batch: Message[] = this.batches[channel];
-
-    if (!batch) {
-      this.batches[channel] = batch = [];
-    }
-
-    if (message instanceof Array) {
-      for (let i: number = 0, len: number = message.length; i < len; i++) {
-        batch.push({ userId, message: message[i] });
-      }
+    if (channelBatch) {
+      channelBatch.push([user, message]);
     } else {
-      batch.push({ userId, message });
+      this.channelsBatches[channel] = [[user, message]];
+    }
+
+    if (this.options.sync) {
+      // if sync is true we want to flush immediately
+      return this.flush();
+    }
+
+    if (!this.flushTimeout) {
+      // schedule next flush within specified interval
+      this.flushTimeout = setTimeout(this.boundFlush, this.options.flushInterval);
     }
   }
 
-  private flush(): void {
-    const preparedMessages: any = {};
+  public register(userId: string, listener: Listener): void {
+    this.usersLink[userId] = { listener, channels: [] };
+  }
 
-    // for each key in batches (key is actual channel name)
-    for (const channel in this.batches) {
-      if (true) {
-        // get all users for that channel
-        const users: string[] = this.channels[channel];
+  public subscribe(userId: string, channels: string[]): void {
+    const userInfo: { listener: Listener, channels: string[] } | undefined = this.usersLink[userId];
 
-        // get actual messages which were send from last iteration
-        const batch: Message[] = this.batches[channel];
-        const batchLen: number = batch.length;
-        // make sure we actually have users for that channel
-        if (users) {
-          // for each user we need to create separate messages array
-          for (let j: number = 0, userLen: number = users.length; j < userLen; j++) {
-            const userId: string = users[j];
-            const userSpecificMessages: any[] = [];
+    if (userInfo) {
+      for (let i: number = 0, len: number = channels.length; i < len; i++) {
+        const channel: string = channels[i];
+        let channelUsersObject: { len: number, [key: string]: string | number } | undefined = this.channelsUsers[channel];
 
-            // make sure we do not send message to the user if user was actual publisher
-            for (let k: number = 0; k < batchLen; k++) {
-              if (batch[k].userId !== userId) {
-                userSpecificMessages.push(batch[k].message);
-              }
-            }
+        if (!channelUsersObject) {
+          channelUsersObject = this.channelsUsers[channel] = {
+            len: 0,
+          };
+          this.options.onChannelCreated(channel);
+        }
 
-            // if we have messages for this user then add this channel with message to all messages
-            if (userSpecificMessages.length) {
-              if (!preparedMessages[userId]) {
-                preparedMessages[userId] = {};
-              }
-              preparedMessages[userId][channel] = userSpecificMessages;
-            }
-          }
-        } else {
-          // this scenario only if we trying to send message to the channel
-          // which does not exist in current server but may exist in some other
-          const brokerMessage: any[] = [];
-          for (let i: number = 0; i < batchLen; i++) {
-            brokerMessage.push(batch[i].message);
-          }
-          // check if broker actually exists in prepared messages
-          if (!preparedMessages['#broker']) {
-            preparedMessages['#broker'] = {};
-          }
-          preparedMessages['#broker'][channel] = brokerMessage;
+        if (!channelUsersObject[userId]) {
+          userInfo.channels.push(channel);
+          channelUsersObject.len++;
+          channelUsersObject[userId] = '1';
         }
       }
     }
+  }
 
-    // clean up all batches
-    this.batches = {};
+  public unregister(userId: string): void {
+    const userInfo: { listener: Listener, channels: string[] } | undefined = this.usersLink[userId];
+    if (userInfo) {
+      this.unsubscribe(userId, [...userInfo.channels]);
+      delete this.usersLink[userId];
+    }
+  }
 
-    // now send message to each user which should receive them
-    for (const userId in preparedMessages) {
-      if (this.users[userId]) {
-        this.users[userId](preparedMessages[userId]);
+  public unsubscribe(userId: string, channels: string[]): void {
+    const userInfo: { listener: Listener, channels: string[] } | undefined = this.usersLink[userId];
+    if (userInfo) {
+      for (let i: number = 0, len: number = channels.length; i < len; i++) {
+        const channel: string = channels[i];
+        const channelUsersObject: { len: number, [key: string]: string | number } | undefined = this.channelsUsers[channel];
+
+        if (channelUsersObject && channelUsersObject[userId]) {
+          channelUsersObject.len--;
+          delete channelUsersObject[userId];
+
+          userInfo.channels.splice(findIndexOf(userInfo.channels, channel), 1);
+
+          if (isObjectEmpty(channelUsersObject)) {
+            delete this.channelsUsers[channel];
+            this.options.onChannelDestroyed(channel);
+          }
+        }
       }
     }
   }
 
-  // run pub sub engine in loop in specific interval
-  private run(): void {
-    setTimeout(() => {
-      this.flush();
-      this.run();
-    }, this.interval);
+  public getStats(): any {
+    // TODO: add more useful stats
+    // let maxUsersPerChannel: number = 0;
+
+    // for (const channel in this.channelsUsers) {
+    //   const subscribed: number = Object.keys(this.channelsUsers[channel]).length;
+    //   if (subscribed > maxUsersPerChannel) {
+    //     maxUsersPerChannel = subscribed;
+    //   }
+    // }
+
+    return {
+      numberOfUsers: Object.keys(this.usersLink).length,
+      numberOfChannels: Object.keys(this.channelsUsers).length,
+    };
+  }
+
+  private flush(): void {
+    const messagesToPublish: any = {};
+
+    for (const channel in this.channelsBatches) {
+      const messages: any[] = this.channelsBatches[channel] || [];
+
+      for (const userId in this.channelsUsers[channel] || {}) {
+        const userMessages: any[] = this.extractUserMessages(userId, messages);
+        if (userMessages.length) {
+          if (!messagesToPublish[userId]) {
+            messagesToPublish[userId] = {};
+          }
+
+          messagesToPublish[userId][channel] = userMessages;
+        }
+      }
+
+      // generate messages for default user '*'
+      // which listens to all messages except it is own
+      const startUserMessages: any[] = this.extractUserMessages(PubSubEngine.GLOBAL_USER, messages);
+      if (startUserMessages.length) {
+        if (!messagesToPublish[PubSubEngine.GLOBAL_USER]) {
+          messagesToPublish[PubSubEngine.GLOBAL_USER] = {};
+        }
+
+        messagesToPublish[PubSubEngine.GLOBAL_USER][channel] = startUserMessages;
+      }
+    }
+
+    // reset flush for next iteration
+    this.channelsBatches = {};
+    this.flushTimeout = null;
+
+    for (const userId in messagesToPublish) {
+      const user: { listener: Listener, channels: string[] } | undefined = this.usersLink[userId];
+      if (user) {
+        user.listener(messagesToPublish[userId]);
+      }
+    }
+  }
+
+  private extractUserMessages(userId: string, messages: any[]): any[] {
+    const messagesForUserInChannel: any[] = [];
+    for (let i: number = 0, len: number = messages.length; i < len; i++) {
+      const message: any[] = messages[i];
+
+      // exclude messages which belong to this user
+      if (message[0] !== userId) {
+        messagesForUserInChannel.push(message[1]);
+      }
+    }
+
+    return messagesForUserInChannel;
   }
 }
+
+// Tests TODO: need to move all test to separate files
+const pubSubEngine: PubSubEngine = new PubSubEngine({ sync: false });
+
+console.log('Start testing "Registration and Subscribing"');
+
+// For comfortable work with millions of channels we need to increase
+// --max-old-space-size (for large applications good size is 8gb per instance)
+const shifting: number = 20;
+const numberOfUsers: number = 50000;
+const numberOfChannelsPerUser: number = 200;
+
+// TODO: improve configurations for publish
+const publishEvery: number = 15; // if 0 disable publish
+const numberOfMessagesPerChannel: number = 1;
+
+console.time('Registration and Subscribing');
+
+let shiftSubscribe: number = 0;
+for (let i: number = 0; i < numberOfUsers; i++) {
+  pubSubEngine.register(`my_user_number_${i}`, (mgs: any): void => {
+    // TODO: write throughput tests
+  });
+
+  for (let j: number = 0 + shiftSubscribe; j < numberOfChannelsPerUser + shiftSubscribe; j++) {
+    pubSubEngine.subscribe(`my_user_number_${i}`, [`one_of_the_subscribed_channels_${j}`]);
+  }
+
+  shiftSubscribe = shiftSubscribe + shifting;
+}
+
+console.log(pubSubEngine.getStats());
+console.timeEnd('Registration and Subscribing');
+
+// console.log('Start testing "Unregister"');
+// console.time('Unregister');
+
+// for (let i: number = 0; i < numberOfUsers; i++) {
+//   pubSubEngine.unregister(`my_user_number_${i}`);
+// }
+
+// console.log(pubSubEngine.getStats());
+// console.timeEnd('Unregister');
+
+// console.log('Start testing "Unsubscribe"');
+// console.time('Unsubscribe');
+// let shiftUnsubscribe: number = 0;
+
+// // This is worst case scenario perf test when indexOf is always at the end
+// for (let i: number = 0; i < numberOfUsers; i++) {
+//   const channels: any[] = [];
+//   for (let j: number = 0 + shiftUnsubscribe; j < numberOfChannelsPerUser + shiftUnsubscribe; j++) {
+//     channels.push(`one_of_the_subscribed_channels_${numberOfChannelsPerUser + shiftUnsubscribe + shiftUnsubscribe - j - 1}`);
+//   }
+
+//   pubSubEngine.unsubscribe(`my_user_number_${i}`, channels);
+//   shiftUnsubscribe = shiftUnsubscribe + shifting;
+// }
+
+// console.log(pubSubEngine.getStats());
+// console.timeEnd('Unsubscribe');
+if (publishEvery) {
+  // TODO: need to improve publish logic
+  let numberOfChannels = pubSubEngine.getStats().numberOfChannels;
+  setInterval(() => {
+    console.time('Publishing data');
+
+    for (let j: number = 0; j < numberOfChannels; j++) {
+      for (let i = 0; i < numberOfMessagesPerChannel; i++) {
+        pubSubEngine.publish(`one_of_the_subscribed_channels_${j}`, { str: "my message" + i });
+      }
+    }
+
+    console.timeEnd('Publishing data');
+  }, publishEvery);
+}
+
+setInterval(() => {
+  console.log(pubSubEngine.getStats());
+  // handle stuff here
+}, 20000);
